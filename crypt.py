e463e5075f881ae6b1b29eac75f9b947712b4c6e
limelier <tudor.iacobescu.1@gmail.com>
2020-10-31 18:32:42 +0200

    make ecb encryption into class to avoid reinstantiating cipher

--- crypt.py
+++ crypt.py
@@ -4,50 +4,52 @@ from typing import List
 from Crypto.Cipher import AES
 
 
-def null_pad(byte_str: bytes, block_size: int) -> bytes:
-    padding_len = block_size - (len(byte_str) % block_size)
+class CipherECB:
+    def __init__(self, key: bytes):
+        self._key: bytes = key
+        self._cipher: AES = AES.new(key, AES.MODE_ECB)
 
-    if padding_len == block_size:
-        return byte_str
-    else:
-        padding = padding_len * b'\0'
-        return byte_str + padding
+    @staticmethod
+    def _null_pad(byte_str: bytes, block_size: int) -> bytes:
+        padding_len = block_size - (len(byte_str) % block_size)
 
+        if padding_len == block_size:
+            return byte_str
+        else:
+            padding = padding_len * b'\0'
+            return byte_str + padding
 
-def null_unpad(padded_str: bytes) -> bytes:
-    padding_len = 0
-    while padded_str[-(1 + padding_len)] == 0x00:
-        padding_len += 1
+    @staticmethod
+    def _null_unpad(padded_str: bytes) -> bytes:
+        padding_len = 0
+        while padded_str[-(1 + padding_len)] == 0x00:
+            padding_len += 1
 
-    if padding_len == 0:
-        return padded_str
-    else:
-        return padded_str[:-padding_len]
+        if padding_len == 0:
+            return padded_str
+        else:
+            return padded_str[:-padding_len]
 
+    @staticmethod
+    def _split_blocks(byte_str: bytes, block_size: int) -> List[bytes]:
+        return [
+            byte_str[i:i + block_size]
+            for i in range(0, len(byte_str), block_size)
+        ]
 
-def split_blocks(byte_str: bytes, block_size: int) -> List[bytes]:
-    return [
-        byte_str[i:i+block_size]
-        for i in range(0, len(byte_str), block_size)
-    ]
+    def encrypt(self, plaintext: bytes) -> bytes:
+        plaintext = self._null_pad(plaintext, 16)
+        plain_blocks = self._split_blocks(plaintext, 16)
+        enc_blocks = [self._cipher.encrypt(block) for block in plain_blocks]
+        encrypted = b''.join(enc_blocks)
+        return encrypted
 
-
-def encrypt_ecb(key: bytes, plaintext: bytes) -> bytes:
-    plaintext = null_pad(plaintext, 16)
-    cipher = AES.new(key, AES.MODE_ECB)
-    plain_blocks = split_blocks(plaintext, 16)
-    enc_blocks = [cipher.encrypt(block) for block in plain_blocks]
-    encrypted = b''.join(enc_blocks)
-    return encrypted
-
-
-def decrypt_ecb(key: bytes, ciphertext: bytes) -> bytes:
-    cipher = AES.new(key, AES.MODE_ECB)
-    cipher_blocks = split_blocks(ciphertext, 16)
-    dec_blocks = [cipher.decrypt(block) for block in cipher_blocks]
-    decrypted = b''.join(dec_blocks)
-    decrypted = null_unpad(decrypted)
-    return decrypted
+    def decrypt(self, ciphertext: bytes) -> bytes:
+        cipher_blocks = self._split_blocks(ciphertext, 16)
+        dec_blocks = [self._cipher.decrypt(block) for block in cipher_blocks]
+        decrypted = b''.join(dec_blocks)
+        decrypted = self._null_unpad(decrypted)
+        return decrypted
 
 
 class CipherOFB:
@@ -80,6 +82,14 @@ class CipherOFB:
 
 def testing():
     key = os.urandom(16)
+
+    # ecb
+    cipher = CipherECB(key)
+    enc = cipher.encrypt(b'hello, world!')
+    dec = cipher.decrypt(enc)
+    print(dec)
+
+    # ofb
     encryptor = CipherOFB(key, b'0' * 16)
     enc = encryptor.transform(b'hello, world!')
     decryptor = CipherOFB(key, b'0' * 16)
